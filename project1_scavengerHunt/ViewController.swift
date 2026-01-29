fb3c8f6cd68a4fe06235bc4840c725a35d5321df
Andy Espinoza <andyesp831@gmail.com>
2026-01-29 14:05:45 -0800

    Initial Commit

---
+++ project1_scavengerHunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1_scavengerHunt
+//
+//  Created by Andy Espinoza on 1/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
